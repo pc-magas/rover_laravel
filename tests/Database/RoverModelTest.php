361584d4977372a004dcf11c479f4ca1b20697d2
Dimitrios Desyllas <pc-magas@users.noreply.github.com>
2019-06-24 14:27:26 +0300

    Test mutators

--- tests/Database/RoverModelTest.php
+++ tests/Database/RoverModelTest.php
@@ -25,29 +25,60 @@ class RoverModelTest extends TestCase
         return $rover;
     }
 
-    private function SetGridPosXValueThrowsExceptionInvalidValueTest($value): void{
+    /**
+     * Test the mutator that on an invalid param will throw Exception
+     *
+     * @param integer $value The provided mutator value
+     * @param boolean $yval Wherther the value is the y position of the rover or not.
+     * @return void
+     */
+    private function setGridPosValueThrowsExceptionInvalidValueTest(int $value, bool $yval=false): void{
         $rover = $this->genRoverForTesting();
         $this->expectException(\InvalidArgumentException::class);
-        $rover->setGridPosXValue($value);
+        if(!$yval){
+            $rover->setGridPosXValue($value);
+        }
+        $rover->setGridPosYValue($value);
     }
 
-    public function testSetGridPosXValueThrowsExceptionWhenNegative(): void {
-        $this->SetGridPosXValueThrowsExceptionInvalidValueTest(TEST_ROVER_MAX_GRID_SIZE_VAL);
-    }
+    private function setGridPosValueValidValueTest(int $value, bool $yval=false): void {
 
-
-
-    public function testSetGridPosXValueThrowsExceptionWhenOverWidth(): void {
-        $this->SetGridPosXValueThrowsExceptionInvalidValueTest(TEST_ROVEL_NEGATIVE_VAL);
-    }
-
-    public function testSetGridPosXValueThorwsExceptionOnNormal(): void {
         $rover = $this->genRoverForTesting();
         try {
-            $rover->setGridPosXValue(TEST_ROVEL_SUCCESS_VAL);
+            if(!$yval){
+                $rover->setGridPosXValue($value);
+            } else {
+                $rover->setGridPosYValue($value);
+            }
             $this->assertTrue(true);
         } catch(\Exception $e) {
             $this->fail("No exception should be thrown when setting a correct value. Exception type: "+get_class($e)+"\n Exception Message: "+$e->getMessage());
         }
+    }
+
+    // For X value
+    public function testSetGridPosXValueThrowsExceptionWhenNegative(): void {
+        $this->setGridPosValueThrowsExceptionInvalidValueTest(TEST_ROVER_MAX_GRID_SIZE_VAL);
+    }
+
+    public function testSetGridPosXValueThrowsExceptionWhenOverWidth(): void {
+        $this->setGridPosValueThrowsExceptionInvalidValueTest(TEST_ROVEL_NEGATIVE_VAL);
+    }
+
+    public function testSetGridPosXValueThorwsExceptionOnNormal(): void {
+        $this->setGridPosValueValidValueTest(TEST_ROVEL_SUCCESS_VAL);
+    }
+
+    // For Y value
+    public function testSetGridPosYValueThrowsExceptionWhenNegative(): void {
+        $this->setGridPosValueThrowsExceptionInvalidValueTest(TEST_ROVER_MAX_GRID_SIZE_VAL,true);
+    }
+
+    public function testSetGridPosYValueThrowsExceptionWhenOverWidth(): void {
+        $this->setGridPosValueThrowsExceptionInvalidValueTest(TEST_ROVEL_NEGATIVE_VAL,true);
+    }
+
+    public function testSetGridPosYValueThorwsExceptionOnNormal(): void {
+        $this->setGridPosValueValidValueTest(TEST_ROVEL_SUCCESS_VAL,true);
     }
 }
